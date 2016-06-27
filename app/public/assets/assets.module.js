6404f09d5298ef89e0ddbdf692e1414e98d7157a
Helge Hofstad <helge.hofstad@gmail.com>
2016-06-27 23:23:32 +0200

    add assets module and icon

---
+++ app/public/assets/assets.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('WallpaperCurator.assets', []);
+
+angular.module('WallpaperCurator.assets')
+
+.factory('Assets', function AssetsFactory() {
+	return {
+		trayIcon: '/icon.png'
+	};
+});
